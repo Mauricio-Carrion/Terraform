16b09978b7dccf943efa409968de3f9d61757cb7
Mauricio Carrion <mauricio.sjn@gmail.com>
2023-01-27 13:38:13 -0300

    add kluster

--- main.tf
+++ main.tf
@@ -9,12 +9,12 @@ terraform {
 provider "google" {
   credentials = file("./poised-octane-375919-c7f0e79b6315.json")
   project     = "poised-octane-375919"
-  region      = "southamerica-east1"
+  region      = "us-central1"
 }
 resource "google_compute_instance" "default" {
   name         = "jenkins-vm"
   machine_type = "f1-micro"
-  zone         = "southamerica-east1-a"
+  zone         = "us-central1-a"
   boot_disk {
     initialize_params {
       image = "ubuntu-os-cloud/ubuntu-2204-lts"
@@ -24,9 +24,30 @@ resource "google_compute_instance" "default" {
     network = "default"
 
     access_config {}
-
   }
+}
 
+resource "google_container_cluster" "k8s" {
+  name                     = "k8s"
+  location                 = "us-central1"
+  remove_default_node_pool = true
+  initial_node_count       = 1
+}
+
+resource "google_container_node_pool" "k8s_preemptible_nodes" {
+  name       = "k8s-node-pool"
+  location   = "us-central1"
+  cluster    = google_container_cluster.k8s.name
+  node_count = 2
+
+  node_config {
+    preemptible  = true
+    machine_type = "e2-medium"
+  }
+}
+
+output "jenkins-vm-ip" {
+  value = google_compute_instance.default.network_interface.0.network_ip
 }
 
 
